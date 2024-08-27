3fac07016bc8cf811fc1c818effc787e430a0fd2
LindembergueOliveira <101476903+LindembergueFrank@users.noreply.github.com>
2024-08-27 13:28:47 -0300

    :tada: Criando projeto

---
+++ src/main/java/com/projeto_springboot/course/ProjetoComSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.projeto_springboot.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoComSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoComSpringBootApplication.class, args);
+	}
+
+}
