b11d1b89d7a0603e007ddd26eee49d120c03e0c4
LindembergueOliveira <101476903+LindembergueFrank@users.noreply.github.com>
2024-09-02 15:21:47 -0300

    :lipstick: feat: Implementando entity Payment, associação one-to-one

--- src/main/java/com/projeto_springboot/course/config/TestConfig.java
+++ src/main/java/com/projeto_springboot/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projeto_springboot.course.entities.Category;
 import com.projeto_springboot.course.entities.Order;
 import com.projeto_springboot.course.entities.OrderItem;
+import com.projeto_springboot.course.entities.Payment;
 import com.projeto_springboot.course.entities.Product;
 import com.projeto_springboot.course.entities.User;
 import com.projeto_springboot.course.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
