6b7681fa3553370f8a747cc68e2fc0db5005fc10
LindembergueOliveira <101476903+LindembergueFrank@users.noreply.github.com>
2024-09-03 13:01:08 -0300

    :lipstick: feat: Tratamento de execeção - findById

--- src/main/java/com/projeto_springboot/course/services/UserService.java
+++ src/main/java/com/projeto_springboot/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.projeto_springboot.course.entities.User;
 import com.projeto_springboot.course.repository.UserRepository;
+import com.projeto_springboot.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> usr = repository.findById(id);
-		return usr.get();
+		return usr.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User user) {
